5d096a82c4a628e23a99702800acddab0f0748f9
Andy Muir <muirandy@yahoo.co.uk>
2019-02-27 10:02:18 +0000

    Added scripts to reset kafka (delete topics, logs etc) and another to create the topics.

---
+++ resetKafka.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ../kafka_2.12-2.1.0
+rm -rf /tmp/kafka*
+rm -rf /tmp/zookeeper*
+rm logs/*
---
+++ setupKafka.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd ../kafka_2.12-2.1.0
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partitions 1 --topic incoming.op.msgs
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partitions 1 --topic modify.op.msgs
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partitions 1 --topic enriched.modification.instructions
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partitions 1 --topic switch.modification.instructions
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partitions 1 --topic service.events
