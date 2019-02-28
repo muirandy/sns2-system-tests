23c8d6ce5f6511199869d2926f3e1ec7cd8635c7
Andy Muir <muirandy@yahoo.co.uk>
2019-02-28 07:56:24 +0000

    Key the messages by orderId. Made more of the payload dynamic.

--- setupKafka.sh
+++ setupKafka.sh
@@ -5,4 +5,4 @@ bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 -
 bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partitions 1 --topic modify.op.msgs
 bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partitions 1 --topic enriched.modification.instructions
 bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partitions 1 --topic switch.modification.instructions
-bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partitions 1 --topic service.events
+#bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partitions 1 --topic service.events
--- src/test/scala/sns/lando/system/ModifyFeatureSpec.scala
+++ src/test/scala/sns/lando/system/ModifyFeatureSpec.scala
@@ -8,6 +8,7 @@ import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, Produce
 import org.scalatest.{FunSpec, GivenWhenThen}
 
 import scala.collection.JavaConverters._
+import scala.util.Random
 
 class ModifyFeatureSpec extends FunSpec with GivenWhenThen {
 
@@ -26,10 +27,13 @@ class ModifyFeatureSpec extends FunSpec with GivenWhenThen {
   props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, KafkaDeserializer)
   props.put(ConsumerConfig.GROUP_ID_CONFIG, this.getClass.getName)
 
+  var messageUuid = UUID.randomUUID().toString
+  var orderId = Random.nextLong().toString
+  var opereratorOrderId = Random.nextString(12)
 
   var expectedKnitwareInstruction =
-    """|<?xml version="1.0" encoding="UTF-8"?>
-      |<switchServiceModificationInstruction switchServiceId="16" netstreamCorrelationId="33269793">
+    s"""|<?xml version="1.0" encoding="UTF-8"?>
+      |<switchServiceModificationInstruction switchServiceId="16" netstreamCorrelationId="${orderId}">
       |  <features>
       |    <callerDisplay active="true"/>
       |    <ringBack active="true"/>
@@ -42,16 +46,16 @@ class ModifyFeatureSpec extends FunSpec with GivenWhenThen {
     it("should update switch (Knitware)") {
       Given("A valid LLU-Stream Modify Features Message")
       val messageValue =
-        """|<?xml version="1.0" encoding="UTF-8"?>
+        s"""|<?xml version="1.0" encoding="UTF-8"?>
           |<transaction receivedDate="2018-11-15T10:29:07" operatorId="sky" operatorTransactionId="op_trans_id_095025_228" operatorIssuedDate="2011-06-01T09:51:12">
           |  <instruction version="1" type="PlaceOrder">
           |    <order>
           |      <type>modify</type>
-          |      <operatorOrderId>SogeaVoipModify_YHUORO</operatorOrderId>
+          |      <operatorOrderId>SogeaVoipModify_${opereratorOrderId}</operatorOrderId>
           |      <operatorNotes>Test: notes</operatorNotes>
-          |      <orderId>33269793</orderId>
+          |      <orderId>${orderId}</orderId>
           |    </order>
-          |    <modifyFeaturesInstruction serviceId="31642339" operatorOrderId="SogeaVoipModify_YHUORO" operatorNotes="Test: addThenRemoveStaticIpToAnFttcService">
+          |    <modifyFeaturesInstruction serviceId="31642339" operatorOrderId="SogeaVoipModify_${opereratorOrderId}" operatorNotes="Test: addThenRemoveStaticIpToAnFttcService">
           |      <features>
           |          <feature code="CallerDisplay"/>
           |          <feature code="RingBack"/>
@@ -61,7 +65,7 @@ class ModifyFeatureSpec extends FunSpec with GivenWhenThen {
           |  </instruction>
           |</transaction>
         """.stripMargin
-      val messageUuid = UUID.randomUUID().toString
+
 
       When("LLU-Stream writes a valid Modify Features Message")
       val consumer = new KafkaConsumer[String, String](props)
@@ -71,7 +75,7 @@ class ModifyFeatureSpec extends FunSpec with GivenWhenThen {
       consumer.seekToBeginning(consumer.assignment)
 
       val lluStreamMessagesTopicProducer = new KafkaProducer[String, String](props)
-      lluStreamMessagesTopicProducer.send(new ProducerRecord(lluStreamMessagesTopic, messageUuid, messageValue)).get()
+      lluStreamMessagesTopicProducer.send(new ProducerRecord(lluStreamMessagesTopic, orderId, messageValue)).get()
 
       lluStreamMessagesTopicProducer.flush()
       lluStreamMessagesTopicProducer.close()
@@ -81,7 +85,7 @@ class ModifyFeatureSpec extends FunSpec with GivenWhenThen {
       val duration = Duration.ofSeconds(2)
       val recs = consumer.poll(duration).asScala
       val last = recs.last
-      assert(last.key() === messageUuid)
+      assert(last.key() === orderId)
       val knitwareInstruction = last.value()
       assert(knitwareInstruction === expectedKnitwareInstruction)
     }
