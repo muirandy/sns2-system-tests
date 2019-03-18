0be97e3898fb25dc8f7b7b7e027ed7aad1c8e3f1
olga.bobrovnytska <olga.bobrovnytska@bskyb.com>
2019-03-18 15:00:12 +0000

    Tidy second set of ksql scripts

--- enhanceVoipSwitchService1.sql
+++ enhanceVoipSwitchService1.sql
@@ -1,10 +1,3 @@
--- CREATE STREAM FROM_VOIP_SWITCH_SERVICES (
---   serviceId BIGINT,
---   switchServiceId BIGINT
--- )
--- WITH (KAFKA_TOPIC='voip-switch-services', VALUE_FORMAT='JSON');
-
-
 CREATE TABLE T_FROM_VOIP_SWITCH_SERVICES (
   serviceId BIGINT,
   switchServiceId BIGINT
