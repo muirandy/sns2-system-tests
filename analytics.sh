798c8306a6dd5fe452e104e79735a135cbe7e7c3
Andy Muir <muirandy@yahoo.co.uk>
2019-03-18 14:49:04 +0000

    Added ElasticSearch and Kibana.

---
+++ analytics.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+curl -X POST -H "Content-Type: application/json" \
+http://localhost:8083/connectors \
+  -d '{
+ "name": "elasticsearch-sink",
+  "config": {
+    "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
+    "tasks.max": "1",
+    "topics": "incoming.op.msgs,switch.modification.instructions",
+    "key.ignore": "true",
+    "topic.schema.ignore":"true",
+    "schema.ignore":"true",
+    "connection.url": "http://elasticsearch:9200",
+    "type.name": "kafka-connect",
+    "name": "elasticsearch-sink"
+  }
+}'
