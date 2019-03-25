6d836fff131361424a7c23cac9806edd2b7a8d12
olga.bobrovnytska <olga.bobrovnytska@bskyb.com>
2019-03-25 12:20:29 +0000

    Added a template to the elasticSearcxh index for audit. Enforces reading the TIMESTAMP field as a date.

--- analytics.sh
+++ analytics.sh
@@ -1,13 +1,72 @@
 #!/usr/bin/env bash
 
+curl -X PUT "localhost:9200/_template/template_1" -H 'Content-Type: application/json' -d'
+{
+  "index_patterns": [
+    "audit"
+  ],
+  "settings": {
+    "number_of_shards": 5
+  },
+  "mappings": {
+    "kafka-connect": {
+      "properties": {
+        "ASSOCIATION": {
+          "type": "text",
+          "fields": {
+            "keyword": {
+              "type": "keyword",
+              "ignore_above": 256
+            }
+          }
+        },
+        "EVENT": {
+          "type": "text",
+          "fields": {
+            "keyword": {
+              "type": "keyword",
+              "ignore_above": 256
+            }
+          }
+        },
+        "ORDER_ID": {
+          "type": "text",
+          "fields": {
+            "keyword": {
+              "type": "keyword",
+              "ignore_above": 256
+            }
+          }
+        },
+        "TIMESTAMP": {
+          "type": "date",
+          "format": "yyyy-MM-dd HH:mm:ss.SSS",
+          "fields": {
+            "keyword": {
+              "type": "keyword",
+              "ignore_above": 256
+            }
+          }
+        }
+      }
+    }
+  }
+}
+'
+
 curl -X POST -H "Content-Type: application/json" \
 http://localhost:8083/connectors \
   -d '{
  "name": "elasticsearch-sink",
   "config": {
+    "transforms": "",
+    "transforms.ConvertTimeValue.type": "org.apache.kafka.connect.transforms.TimestampConverter$Value",
+    "transforms.ConvertTimeValue.target.type": "Timestamp",
+    "transforms.ConvertTimeValue.field": "TIMESTAMP",
+    "transforms.ConvertTimeValue.format": "yyyy-MM-dd HH:mm:ss.SSS",
     "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
     "tasks.max": "1",
-    "topics": "AUDIT,modify.op.msgs,SINK_MODIFY_VOIP_INSTRUCTIONS_WITH_SWITCH_ID",
+    "topics": "AUDIT",
     "key.ignore": "true",
     "topic.schema.ignore":"true",
     "schema.ignore":"true",
