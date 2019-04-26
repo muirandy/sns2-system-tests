0b1eec0e03c12009030c7aa7e1798debb781149c
Andy Muir <muirandy@yahoo.co.uk>
2019-04-26 08:59:31 +0100

    Refactor: Writing a Service test for KSQL.

--- src/test/java/sns.lando.service/ModifyVoiceParserShould.java
+++ src/test/java/sns.lando.service/ModifyVoiceParserShould.java
@@ -57,41 +57,22 @@ public class ModifyVoiceParserShould {
     private static final KafkaContainer KAFKA_CONTAINER = new KafkaContainer("5.2.1").withEmbeddedZookeeper()
                                                                                      .waitingFor(Wait.forLogMessage(".*started.*\\n", 1));
 
-//    @Container
-//    private GenericContainer ksqlContainer = new GenericContainer("sns2-system-tests_kafka-ksql:latest")
-//            .withEnv(calculateKsqlEnvProperties())
-//            .withNetwork(KAFKA_CONTAINER.getNetwork())
-//            .withClasspathResourceMapping("kafka-ksql/scripts/modifyVoice.sql", "/opt/kafka-ksql/scripts/modifyVoice.sql", BindMode.READ_ONLY)
-//            .waitingFor(Wait.forHttp("http://localhost:8088/"))
-//            .waitingFor(Wait.forLogMessage(".*INFO Server up and running.*\\n", 1));
-
+    @Container
+    private GenericContainer ksqlContainer = new GenericContainer("sns2-system-tests_kafka-ksql:latest")
+            .withEnv(calculateKsqlEnvProperties())
+            .withNetwork(KAFKA_CONTAINER.getNetwork())
+            .withClasspathResourceMapping("kafka-ksql/scripts/modifyVoice.sql", "/opt/kafka-ksql/scripts/modifyVoice.sql", BindMode.READ_ONLY)
+            .waitingFor(Wait.forHttp("http://localhost:8088/"))
+            .waitingFor(Wait.forLogMessage(".*INFO Server up and running.*\\n", 1));
     private String orderId = generateRandomString();
 
     private KafkaConsumer<String, String> consumer;
-    private GenericContainer ksqlContainer;
-
-    private Map<String, String> calculateKsqlEnvProperties() {
-        Map<String, String> envProperties = new HashMap<>();
-        envProperties.put(ENV_KEY_KSQL_KSQL_SERVICE_ID, "ksql-server-modifyParserShould");
-        envProperties.put(ENV_KEY_KSQL_KSQL_QUERIES_FILE, "/opt/kafka-ksql/scripts/modifyVoice.sql");
-        envProperties.put(ENV_KEY_KSQL_BOOTSTRAP_SERVERS, KAFKA_CONTAINER.getNetworkAliases().get(0) + ":9092");
-        envProperties.put(ENV_KEY_KAFKA_BROKER_SERVER, KAFKA_CONTAINER.getNetworkAliases().get(0));
-        envProperties.put(ENV_KEY_KAFKA_BROKER_PORT, "" + 9092);
-        envProperties.put(ENV_KEY_KSQL_KSQL_SINK_REPLICAS, "1");
-        envProperties.put(ENV_KEY_KSQL_KSQL_SINK_PARTITIONS, "1");
-        envProperties.put(ENV_KEY_KSQL_KSQL_STREAMS_REPLICATION_FACTOR, "1");
-        envProperties.put("KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR", "2");
-        envProperties.put("ksql.sink.partitions", "1");
-        envProperties.put("KSQL_LISTENERS", "http://0.0.0.0:8088");
-
-        return envProperties;
-    }
 
     @BeforeEach
     public void setup() {
         assertTrue(KAFKA_CONTAINER.isRunning());
-        createTopics();
-        startupKsql();
+        //        createTopics();
+        //        startupKsql();
         assertTrue(ksqlContainer.isRunning());
     }
 
@@ -109,42 +90,23 @@ public class ModifyVoiceParserShould {
         }
     }
 
-    @AfterEach
-    public void tearDown() {
-        System.out.println("Kafka Logs = " + KAFKA_CONTAINER.getLogs());
-        System.out.println("Converter Logs = " + ksqlContainer.getLogs());
-        ksqlContainer.stop();
-    }
+    private Map<String, String> calculateKsqlEnvProperties() {
+        createTopics();
 
-    @Test
-    public void convertsAnyXmlToJson() throws ExecutionException, InterruptedException {
+        Map<String, String> envProperties = new HashMap<>();
+        envProperties.put(ENV_KEY_KSQL_KSQL_SERVICE_ID, "ksql-server-modifyParserShould");
+        envProperties.put(ENV_KEY_KSQL_KSQL_QUERIES_FILE, "/opt/kafka-ksql/scripts/modifyVoice.sql");
+        envProperties.put(ENV_KEY_KSQL_BOOTSTRAP_SERVERS, KAFKA_CONTAINER.getNetworkAliases().get(0) + ":9092");
+        envProperties.put(ENV_KEY_KAFKA_BROKER_SERVER, KAFKA_CONTAINER.getNetworkAliases().get(0));
+        envProperties.put(ENV_KEY_KAFKA_BROKER_PORT, "" + 9092);
+        envProperties.put(ENV_KEY_KSQL_KSQL_SINK_REPLICAS, "1");
+        envProperties.put(ENV_KEY_KSQL_KSQL_SINK_PARTITIONS, "1");
+        envProperties.put(ENV_KEY_KSQL_KSQL_STREAMS_REPLICATION_FACTOR, "1");
+        envProperties.put("KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR", "2");
+        envProperties.put("ksql.sink.partitions", "1");
+        envProperties.put("KSQL_LISTENERS", "http://0.0.0.0:8088");
 
-        Thread.sleep(1000);
-
-        writeXmlToInputTopic();
-
-        assertKafkaMessageEquals();
-    }
-
-    @NotNull
-    private String generateRandomString() {
-        return String.valueOf(new Random().nextLong());
-    }
-
-    private void writeXmlToInputTopic() throws InterruptedException, ExecutionException {
-        new KafkaProducer<String, String>(getKafkaProperties()).send(createKafkaProducerRecord(orderId)).get();
-    }
-
-    @NotNull
-    private ProducerRecord createKafkaProducerRecord(String orderId) {
-        return new ProducerRecord(INPUT_TOPIC, orderId, createMessage(orderId));
-    }
-
-    private String formatExpectedValue(String orderId) {
-        return String.format(
-                "{\"TRACE_ID\":\"${json-unit.ignore}\",\"OPERATOR_ID\":\"sky\",\"OPERATOR_ORDER_ID\":\"SogeaVoipModify_-9156304217878863645\",\"ORDER_ID\":\"%s\",\"SERVICE_ID\":\"31642339\",\"FEATURES\":[{\"code\":\"CallerDisplay\"},{\"code\":\"RingBack\"},{\"code\":\"ChooseToRefuse\"}]}",
-                orderId
-        );
+        return envProperties;
     }
 
     private void createTopics() {
@@ -170,6 +132,49 @@ public class ModifyVoiceParserShould {
             }
         });
         adminClient.close();
+    }
+
+    private static Properties getKafkaProperties() {
+        String bootstrapServers = KAFKA_CONTAINER.getBootstrapServers();
+
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put("acks", "all");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KAFKA_SERIALIZER);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KAFKA_SERIALIZER);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KAFKA_DESERIALIZER);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, KAFKA_DESERIALIZER);
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, ModifyVoiceParserShould.class.getName());
+        return props;
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.out.println("Kafka Logs = " + KAFKA_CONTAINER.getLogs());
+        System.out.println("Converter Logs = " + ksqlContainer.getLogs());
+        ksqlContainer.stop();
+    }
+
+    @Test
+    public void convertsAnyXmlToJson() throws ExecutionException, InterruptedException {
+
+        Thread.sleep(1000);
+
+        writeXmlToInputTopic();
+
+        assertKafkaMessageEquals();
+    }
+
+    private void writeXmlToInputTopic() throws InterruptedException, ExecutionException {
+        new KafkaProducer<String, String>(getKafkaProperties()).send(createKafkaProducerRecord(orderId)).get();
+    }
+
+    @NotNull
+    private ProducerRecord createKafkaProducerRecord(String orderId) {
+        return new ProducerRecord(INPUT_TOPIC, orderId, createMessage(orderId));
     }
 
     private String createMessage(String orderId) {
@@ -224,23 +229,6 @@ public class ModifyVoiceParserShould {
             fail("Did not find expected record");
     }
 
-    private Properties getKafkaProperties() {
-        String bootstrapServers = KAFKA_CONTAINER.getBootstrapServers();
-
-        Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put("acks", "all");
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KAFKA_SERIALIZER);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KAFKA_SERIALIZER);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KAFKA_DESERIALIZER);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, KAFKA_DESERIALIZER);
-        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, this.getClass().getName());
-        return props;
-    }
-
     private ConsumerRecords<String, String> pollForResults() {
         consumer = createKafkaConsumer();
         Duration duration = Duration.ofSeconds(8);
@@ -267,4 +255,17 @@ public class ModifyVoiceParserShould {
         String expectedValue = formatExpectedValue(orderId);
         assertJsonEquals(expectedValue, value);
     }
+
+    private String formatExpectedValue(String orderId) {
+        return String.format(
+                "{\"TRACE_ID\":\"${json-unit.ignore}\",\"OPERATOR_ID\":\"sky\",\"OPERATOR_ORDER_ID\":\"SogeaVoipModify_-9156304217878863645\",\"ORDER_ID\":\"%s\",\"SERVICE_ID\":\"31642339\",\"FEATURES\":[{\"code\":\"CallerDisplay\"},{\"code\":\"RingBack\"},{\"code\":\"ChooseToRefuse\"}]}",
+                orderId
+        );
+    }
+
+    @NotNull
+    private String generateRandomString() {
+        return String.valueOf(new Random().nextLong());
+    }
+
 }
