e1f7e1f6745548272d3f52f31f10bfcd6c432ea5
Andy Muir <andy.muir@aimyourtechnology.com>
2019-09-05 11:53:03 +0100

    Developing the System Test. Write Service and SwitchService data to Kafka topic directly. We don't have access to Oracle DB image, so no point in running connect.

--- src/test/java/sns/lando/system/SystemShould.java
+++ src/test/java/sns/lando/system/SystemShould.java
@@ -1,9 +1,13 @@
 package sns.lando.system;
 
+import com.eclipsesource.json.JsonObject;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.jetbrains.annotations.NotNull;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,10 +15,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SystemShould extends AmqSinkTestBase {
 
     private static final String SERVICES_TOPIC = "services";
+    private static final String SWITCH_SERVICES_TOPIC = "voip-switch-services";
+
+    private Integer voipServiceId = 100;
+    private String directoryNumber = "0123456001";
+    private Integer voipSwitchServiceId;
 
     @Before
     public void setUp() {
+        createNewIdsForTest();
         configureActiveMqSinkConnector();
+    }
+
+    @After
+    private void createNewIdsForTest() {
+        voipServiceId++;
+        voipSwitchServiceId++;
+        directoryNumber = calculateNextDirectoryNumber();
+    }
+
+    private String calculateNextDirectoryNumber() {
+        int current = Integer.getInteger(directoryNumber);
+        current++;
+        return "0" + current;
     }
 
     @Test
@@ -40,8 +63,7 @@ public class SystemShould extends AmqSinkTestBase {
 
     private void createVoipServiceDirectlyInKafka() {
         try {
-            KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(kafkaPropertiesForProducer());
-            kafkaProducer.send(createProducerRecord(SERVICES_TOPIC, buildServiceKey(), buildServiceMessage())).get();
+            getStringStringKafkaProducer().send(createProducerRecord(SERVICES_TOPIC, buildServiceKey(), buildServiceMessage())).get();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
@@ -51,19 +73,46 @@ public class SystemShould extends AmqSinkTestBase {
 //        lluStreamMessagesTopicProducer.close()
     }
 
-    private String buildServiceKey() {
-        return null;
+    @NotNull
+    private KafkaProducer<String, String> getStringStringKafkaProducer() {
+        return new KafkaProducer<>(kafkaPropertiesForProducer());
     }
 
+    private String buildServiceKey() {
+        return "" + voipServiceId;
+    }
+
+    //{"serviceId":31642339,"serviceSpecCode":"VoipService","directoryNumber":"01202000095"}
     private String buildServiceMessage() {
-        return null;
+        return new JsonObject()
+                .add("serviceId", voipServiceId)
+                .add("serviceSpecCode", "VoipService")
+                .add("directoryNumber", directoryNumber)
+                .toString();
     }
 
     private void createSwitchServiceDirectlyInKafka() {
+        try {
+            getStringStringKafkaProducer().send(createProducerRecord(SWITCH_SERVICES_TOPIC, buildServiceKey(), buildSwitchServiceMessage())).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private String buildSwitchServiceMessage() {
+        return new JsonObject()
+                .add("serviceId", voipServiceId)
+                .add("switchServiceId", voipSwitchServiceId)
+                .toString();
     }
 
     private void modifyVoipFeatures() {
+        writeModifyVoipFeaturesInstructionToActiveMq();
+    }
+
+    private void writeModifyVoipFeaturesInstructionToActiveMq() {
 
     }
 
