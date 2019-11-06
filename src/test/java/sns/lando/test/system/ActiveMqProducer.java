7e090f08df3c7ce7619a8c3f30bea14b66cb2a46
Andy Muir <andy.muir@aimyourtechnology.com>
2019-11-06 14:40:11 +0000

    Handle Active MQ connections better

--- src/test/java/sns/lando/test/system/ActiveMqProducer.java
+++ src/test/java/sns/lando/test/system/ActiveMqProducer.java
@@ -7,6 +7,7 @@ import javax.jms.*;
 class ActiveMqProducer {
     private final String activeMqEndpoint;
     private final String activeMqQueueName;
+    private Connection connection;
 
     ActiveMqProducer(String activeMqEndpoint, String activeMqQueueName) {
         this.activeMqEndpoint = activeMqEndpoint;
@@ -16,8 +17,20 @@ class ActiveMqProducer {
     public void start() {
         ActiveMQConnectionFactory factory = createConnectionFactory(activeMqEndpoint);
         try {
-            Connection connection = factory.createConnection();
+            connection = factory.createConnection();
             connection.start();
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+
+    ActiveMQConnectionFactory createConnectionFactory(String activeMqConnectionString) {
+        return new ActiveMQConnectionFactory(activeMqConnectionString);
+    }
+
+    void stop() {
+        try {
+            connection.close();
         } catch (JMSException e) {
             e.printStackTrace();
         }
@@ -26,7 +39,7 @@ class ActiveMqProducer {
     void write(String message, String traceyId, int orderId) {
         ActiveMQConnectionFactory factory = createConnectionFactory(activeMqEndpoint);
         try {
-            Connection connection = factory.createConnection();
+            //            Connection connection = factory.createConnection();
             Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
             Queue queue = session.createQueue(activeMqQueueName);
             MessageProducer producer = session.createProducer(queue);
@@ -34,12 +47,9 @@ class ActiveMqProducer {
             textMessage.setStringProperty("TRACEY_ID", traceyId);
             textMessage.setStringProperty("JMSXGroupID", "" + orderId);
             producer.send(textMessage);
+            session.close();
         } catch (JMSException e) {
             e.printStackTrace();
         }
-    }
-
-    ActiveMQConnectionFactory createConnectionFactory(String activeMqConnectionString) {
-        return new ActiveMQConnectionFactory(activeMqConnectionString);
     }
 }
