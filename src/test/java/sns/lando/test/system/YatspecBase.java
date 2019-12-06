bbb6b4f13b1c151bb1aaa485909236374f4b43ed
Andy Muir <andy.muir@aimyourtechnology.com>
2019-12-06 22:48:10 +0000

    Add Component Diagram to Yatspec report

--- src/test/java/sns/lando/test/system/YatspecBase.java
+++ src/test/java/sns/lando/test/system/YatspecBase.java
@@ -1,5 +1,6 @@
 package sns.lando.test.system;
 
+import com.github.muirandy.living.artifact.main.ComponentDiagramApp;
 import com.google.common.collect.ImmutableSet;
 import com.googlecode.yatspec.junit.SpecResultListener;
 import com.googlecode.yatspec.junit.SpecRunner;
@@ -11,14 +12,23 @@ import com.googlecode.yatspec.rendering.html.DontHighlightRenderer;
 import com.googlecode.yatspec.rendering.html.HtmlResultRenderer;
 import com.googlecode.yatspec.state.givenwhenthen.TestState;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.function.BiConsumer;
 
 import static com.googlecode.yatspec.plugin.sequencediagram.SequenceDiagramGenerator.getHeaderContentForModalWindows;
 
 @RunWith(SpecRunner.class)
-class YatspecBase extends TestState implements LivingDocumentation, WithCustomResultListeners {
+public class YatspecBase extends TestState implements LivingDocumentation, WithCustomResultListeners {
+
+    private ComponentDiagramApp componentDiagramApp;
+
     @Override
     public BiConsumer<String, Object> getGivenLogConsumer() {
         return (key, value) -> {
@@ -40,14 +50,32 @@ class YatspecBase extends TestState implements LivingDocumentation, WithCustomRe
         };
     }
 
+    @Before
+    public void resetComponentDiagram() {
+        componentDiagramApp = new ComponentDiagramApp(new String[]{"http://jaeger:16686"});
+        componentDiagramApp.initialise();
+    }
+
     @After
     public void tearDown() {
         addSequenceDiagram();
+        addComponentDiagram();
     }
 
     private void addSequenceDiagram() {
         super.log("Sequence diagram", new SequenceDiagramGenerator()
                 .generateSequenceDiagram(new ByNamingConventionMessageProducer().messages(capturedInputAndOutputs)));
+    }
+
+    private void addComponentDiagram() {
+        SvgWrapper componentDiagramSvgWrapper = generateComponentDiagram();
+        super.log("Component Diagram", componentDiagramSvgWrapper);
+    }
+
+    private SvgWrapper generateComponentDiagram() {
+        ByteArrayOutputStream byteArrayOutputStream = componentDiagramApp.drawComponentDiagram();
+        String content = new String(byteArrayOutputStream.toByteArray(), Charset.forName("UTF-8"));
+        return new SvgWrapper(content);
     }
 
     @Override
